d4fe020d39fd6ac8945a212dcbe5040d6c4a59ef
Psychloor <blomqvistkevin@hotmail.com>
2024-11-23 18:20:45 +0100

    Refactor ConcurrentCommandHistory to use RwLock for undo/redo stacks and add methods for retrieving undo/redo history

--- src/concurrent_command_history.rs
+++ src/concurrent_command_history.rs
@@ -7,13 +7,13 @@ use std::{
     },
 };
 
-use parking_lot::Mutex;
+use parking_lot::RwLock;
 
 use crate::traits::{command::Command, command_history::CommandHistory};
 
 pub struct ConcurrentCommandHistory<C: Command + Send + Sync> {
-    undo: Mutex<VecDeque<C>>,
-    redo: Mutex<VecDeque<C>>,
+    undo: RwLock<VecDeque<Arc<C>>>,
+    redo: RwLock<VecDeque<Arc<C>>>,
     history_limit: AtomicUsize,
     clear_redo_on_execute: AtomicBool,
 }
@@ -27,14 +27,36 @@ where
         let limit = history_limit.get();
 
         Arc::new(Self {
-            undo: Mutex::new(VecDeque::with_capacity(limit)),
-            redo: Mutex::new(VecDeque::with_capacity(limit)),
+            undo: RwLock::new(VecDeque::with_capacity(limit)),
+            redo: RwLock::new(VecDeque::with_capacity(limit)),
             history_limit: AtomicUsize::new(limit),
             clear_redo_on_execute: AtomicBool::new(clear_redo_on_execute),
         })
     }
 
-    fn push_undo(&self, command: C, undo_lock: &mut parking_lot::MutexGuard<VecDeque<C>>) {
+    pub fn undo_history(&self) -> Option<Vec<Arc<C>>> {
+        let undo_lock = self.undo.read();
+        if undo_lock.is_empty() {
+            return None;
+        }
+
+        Some(undo_lock.iter().cloned().collect())
+    }
+
+    pub fn redo_history(&self) -> Option<Vec<Arc<C>>> {
+        let redo_lock = self.redo.read();
+        if redo_lock.is_empty() {
+            return None;
+        }
+
+        Some(redo_lock.iter().cloned().collect())
+    }
+
+    fn push_undo(
+        &self,
+        command: Arc<C>,
+        undo_lock: &mut parking_lot::RwLockWriteGuard<VecDeque<Arc<C>>>,
+    ) {
         let limit = self.history_limit.load(Ordering::Relaxed);
         while undo_lock.len() >= limit {
             undo_lock.pop_back();
@@ -43,7 +65,11 @@ where
         undo_lock.push_front(command);
     }
 
-    fn push_redo(&self, command: C, redo_lock: &mut parking_lot::MutexGuard<VecDeque<C>>) {
+    fn push_redo(
+        &self,
+        command: Arc<C>,
+        redo_lock: &mut parking_lot::RwLockWriteGuard<VecDeque<Arc<C>>>,
+    ) {
         let limit = self.history_limit.load(Ordering::Relaxed);
         while redo_lock.len() >= limit {
             redo_lock.pop_back();
@@ -62,32 +88,33 @@ where
     C: Command + Send + Sync,
 {
     fn execute_command(&self, command: C, ctx: &C::Context) {
+        let command = Arc::new(command);
         command.execute(ctx);
 
-        let mut undo = self.undo.lock();
+        let mut undo = self.undo.write();
         self.push_undo(command, &mut undo);
 
         if self.clear_redo_on_execute.load(Ordering::Relaxed) {
-            self.redo.lock().clear();
+            self.redo.write().clear();
         }
     }
 
     fn undo(&self, ctx: &C::Context) {
-        let mut undo = self.undo.lock();
+        let mut undo = self.undo.write();
         if let Some(command) = undo.pop_front() {
             command.undo(ctx);
 
-            let mut redo = self.redo.lock();
+            let mut redo = self.redo.write();
             self.push_redo(command, &mut redo);
         }
     }
 
     fn redo(&self, ctx: &C::Context) {
-        let mut redo = self.redo.lock();
+        let mut redo = self.redo.write();
         if let Some(command) = redo.pop_front() {
             command.redo(ctx);
 
-            let mut undo = self.undo.lock();
+            let mut undo = self.undo.write();
             self.push_undo(command, &mut undo);
         }
     }
@@ -98,34 +125,37 @@ where
 
         self.history_limit.store(limit, Ordering::Release);
 
-        let mut undo = self.undo.lock();
+        let mut undo = self.undo.write();
         while undo.len() > limit {
             undo.pop_back();
         }
 
-        let mut redo = self.redo.lock();
+        let mut redo = self.redo.write();
         while redo.len() > limit {
             redo.pop_back();
         }
     }
 
     fn batch_execute(&self, commands: Vec<C>, ctx: &C::Context) {
-        let mut undo = self.undo.lock();
+        let mut undo = self.undo.write();
         for command in commands {
+            let command = Arc::new(command);
             command.execute(ctx);
 
             self.push_undo(command, &mut undo);
         }
 
         if self.clear_redo_on_execute.load(Ordering::Relaxed) {
-            self.redo.lock().clear();
+            self.redo.write().clear();
         }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use std::{cmp::min, hint::black_box, num::NonZero, panic::AssertUnwindSafe, thread, time};
+    use std::{
+        borrow::Cow, cmp::min, hint::black_box, num::NonZero, panic::AssertUnwindSafe, thread, time,
+    };
 
     use rand::Rng;
 
@@ -133,6 +163,7 @@ mod tests {
 
     use super::*;
 
+    #[derive(Debug)]
     enum TestOperation {
         Increment(i32),
         Decrement(i32),
@@ -161,6 +192,17 @@ mod tests {
             match self.operation {
                 TestOperation::Increment(value) => ctx.value -= value,
                 TestOperation::Decrement(value) => ctx.value += value,
+            }
+        }
+
+        fn description(&self) -> Cow<'_, str> {
+            match self.operation {
+                TestOperation::Increment(value) => {
+                    Cow::Owned(format!("TestArcCommand: Increment({value})"))
+                }
+                TestOperation::Decrement(value) => {
+                    Cow::Owned(format!("TestArcCommand: Decrement({value})"))
+                }
             }
         }
     }
@@ -199,19 +241,19 @@ mod tests {
 
         history.execute_command(command1, &ctx);
         assert_eq!(ctx.lock().value, 1);
-        assert_eq!(history.undo.lock().len(), 1);
-        assert!(history.redo.lock().is_empty());
+        assert_eq!(history.undo.read().len(), 1);
+        assert!(history.redo.read().is_empty());
 
         history.execute_command(command2, &ctx);
         assert_eq!(ctx.lock().value, 3);
-        assert_eq!(history.undo.lock().len(), 2);
-        assert!(history.redo.lock().is_empty());
+        assert_eq!(history.undo.read().len(), 2);
+        assert!(history.redo.read().is_empty());
 
         history.execute_command(command3, &ctx);
 
         assert_eq!(ctx.lock().value, 6);
-        assert_eq!(history.undo.lock().len(), 2);
-        assert!(history.redo.lock().is_empty());
+        assert_eq!(history.undo.read().len(), 2);
+        assert!(history.redo.read().is_empty());
     }
 
     #[test]
@@ -243,7 +285,7 @@ mod tests {
         history.batch_execute(commands, &ctx);
 
         assert_eq!(ctx.lock().value, expected_sum);
-        assert_eq!(history.undo.lock().len(), min(size, 35));
+        assert_eq!(history.undo.read().len(), min(size, 35));
     }
 
     #[test]
@@ -262,22 +304,22 @@ mod tests {
 
         history.execute_command(command1, &ctx);
         assert_eq!(ctx.lock().value, 1);
-        assert_eq!(history.undo.lock().len(), 1);
-        assert!(history.redo.lock().is_empty());
+        assert_eq!(history.undo.read().len(), 1);
+        assert!(history.redo.read().is_empty());
 
         history.execute_command(command2, &ctx);
         assert_eq!(ctx.lock().value, 3);
-        assert_eq!(history.undo.lock().len(), 2);
-        assert!(history.redo.lock().is_empty());
+        assert_eq!(history.undo.read().len(), 2);
+        assert!(history.redo.read().is_empty());
 
         history.execute_command(command3, &ctx);
         assert_eq!(ctx.lock().value, 6);
-        assert_eq!(history.undo.lock().len(), 2);
-        assert!(history.redo.lock().is_empty());
+        assert_eq!(history.undo.read().len(), 2);
+        assert!(history.redo.read().is_empty());
 
         history.set_history_limit(NonZero::new(1).unwrap());
-        assert_eq!(history.undo.lock().len(), 1);
-        assert!(history.redo.lock().is_empty());
+        assert_eq!(history.undo.read().len(), 1);
+        assert!(history.redo.read().is_empty());
     }
 
     #[test]
@@ -314,8 +356,8 @@ mod tests {
         }
 
         assert_eq!(ctx.lock().value, sum);
-        assert_eq!(history.undo.lock().len(), min(size, count));
-        assert!(history.redo.lock().is_empty());
+        assert_eq!(history.undo.read().len(), min(size, count));
+        assert!(history.redo.read().is_empty());
 
         let mut handles = Vec::new();
         for _ in 0..size {
@@ -340,12 +382,12 @@ mod tests {
 
         assert_eq!(ctx.lock().value, sum);
         assert_eq!(
-            history.undo.lock().len(),
+            history.undo.read().len(),
             min(size, count),
             "Undo stack size after execution"
         );
         assert!(
-            history.redo.lock().is_empty(),
+            history.redo.read().is_empty(),
             "Redo stack should be empty after execution"
         );
     }
@@ -363,18 +405,18 @@ mod tests {
 
         history.execute_command(command, &ctx);
         assert_eq!(ctx.lock().value, 1);
-        assert_eq!(history.undo.lock().len(), 1);
-        assert!(history.redo.lock().is_empty());
+        assert_eq!(history.undo.read().len(), 1);
+        assert!(history.redo.read().is_empty());
 
         history.undo(&ctx);
         assert_eq!(ctx.lock().value, 0);
-        assert!(history.undo.lock().is_empty());
-        assert_eq!(history.redo.lock().len(), 1);
+        assert!(history.undo.read().is_empty());
+        assert_eq!(history.redo.read().len(), 1);
 
         history.execute_command(command2, &ctx);
         assert_eq!(ctx.lock().value, 1);
-        assert_eq!(history.undo.lock().len(), 1);
-        assert!(history.redo.lock().is_empty());
+        assert_eq!(history.undo.read().len(), 1);
+        assert!(history.redo.read().is_empty());
     }
 
     #[test]
@@ -390,18 +432,18 @@ mod tests {
 
         history.execute_command(command, &ctx);
         assert_eq!(ctx.lock().value, 1);
-        assert_eq!(history.undo.lock().len(), 1);
-        assert!(history.redo.lock().is_empty());
+        assert_eq!(history.undo.read().len(), 1);
+        assert!(history.redo.read().is_empty());
 
         history.undo(&ctx);
         assert_eq!(ctx.lock().value, 0);
-        assert!(history.undo.lock().is_empty());
-        assert_eq!(history.redo.lock().len(), 1);
+        assert!(history.undo.read().is_empty());
+        assert_eq!(history.redo.read().len(), 1);
 
         history.execute_command(command2, &ctx);
         assert_eq!(ctx.lock().value, 1);
-        assert_eq!(history.undo.lock().len(), 1);
-        assert_eq!(history.redo.lock().len(), 1);
+        assert_eq!(history.undo.read().len(), 1);
+        assert_eq!(history.redo.read().len(), 1);
     }
 
     #[test]
@@ -419,5 +461,59 @@ mod tests {
 
         assert!(result.is_err()); // Ensure panic was caught
         assert_eq!(ctx.lock().value, 1); // Ensure state is consistent
+    }
+
+    #[test]
+    fn test_undo_history() {
+        let history = ConcurrentCommandHistory::new(NonZeroUsize::new(5).unwrap(), true);
+        let ctx = SharedContext::new(TestArcContext { value: 0 });
+        let command1 = TestArcCommand {
+            operation: TestOperation::Increment(1),
+        };
+        let command2 = TestArcCommand {
+            operation: TestOperation::Increment(2),
+        };
+
+        history.execute_command(command1, &ctx);
+        history.execute_command(command2, &ctx);
+
+        let undo_history = history.undo_history().unwrap();
+        assert_eq!(undo_history.len(), 2);
+        assert_eq!(
+            undo_history[0].description(),
+            "TestArcCommand: Increment(2)"
+        );
+        assert_eq!(
+            undo_history[1].description(),
+            "TestArcCommand: Increment(1)"
+        );
+    }
+
+    #[test]
+    fn test_redo_history() {
+        let history = ConcurrentCommandHistory::new(NonZeroUsize::new(5).unwrap(), true);
+        let ctx = SharedContext::new(TestArcContext { value: 0 });
+        let command1 = TestArcCommand {
+            operation: TestOperation::Increment(1),
+        };
+        let command2 = TestArcCommand {
+            operation: TestOperation::Increment(2),
+        };
+
+        history.execute_command(command1, &ctx);
+        history.execute_command(command2, &ctx);
+        history.undo(&ctx);
+        history.undo(&ctx);
+
+        let redo_history = history.redo_history().unwrap();
+        assert_eq!(redo_history.len(), 2);
+        assert_eq!(
+            redo_history[0].description(),
+            "TestArcCommand: Increment(1)"
+        );
+        assert_eq!(
+            redo_history[1].description(),
+            "TestArcCommand: Increment(2)"
+        );
     }
 }
